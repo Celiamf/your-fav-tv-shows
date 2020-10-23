f6b923befdcf86868692087988400b55ed99da96
Celiamf <celiamartinfern@gmail.com>
2020-10-23 13:45:28 +0200

    Update scss for card class

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
 const btn = document.querySelector(".js-searchBtn");
 const rsltContainer = document.querySelector(".js-searchContainer");
+const favContainer = document.querySelector(".js-favContainer");
 const input = document.querySelector(".js-input");
 let results = [];
 
@@ -22,12 +23,12 @@ function renderResults() {
   let filledHtml = "";
   for (let result of results) {
     if (result.show.image === null) {
-      filledHtml += "<li>";
+      filledHtml += '<li class="card js-card">';
       filledHtml += `<h3>${result.show.name}</h3>`;
       filledHtml += `<img src="https://via.placeholder.com/210x296/f0ffff/00008b/?text=No+image+available"/>`;
       filledHtml += "</li>";
     } else {
-      filledHtml += "<li>";
+      filledHtml += '<li class="card">';
       filledHtml += `<h3>${result.show.name}</h3>`;
       filledHtml += `<img src="${result.show.image.medium}"/>`;
       filledHtml += "</li>";
